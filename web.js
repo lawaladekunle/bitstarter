07993deef853e4718a191249a19331b564a47221
lawaladekunle <lawaladekunle@gmail.com>
2013-07-29 07:47:34 +0000

    Appended code to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require("fs");
 var app = express();
 app.use(express.logger());
 
+/* Read contents of index.html*/
+
+var content_fs = fs.readFileSync("index.html", "utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content_fs);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
